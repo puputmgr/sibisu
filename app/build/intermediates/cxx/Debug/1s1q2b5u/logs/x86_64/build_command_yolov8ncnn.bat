f273402969bcc678e8f700231d59877f7815564d
Puput Mangge Rahayu <puputmangge123@gmail.com>
2024-05-31 19:11:32 +0700

    Pesan commit Anda di sini

---
+++ app/build/intermediates/cxx/Debug/1s1q2b5u/logs/x86_64/build_command_yolov8ncnn.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\tegar\\AppData\\Local\\Android\\Sdk\\cmake\\3.10.2.4988404\\bin\\ninja.exe" ^
+  -C ^
+  "D:\\Lectures\\LE - In Progress\\SEKERIPSHI\\Projek Skripsi\\ncnn-android-yolov8-main\\ncnn-android-yolov8\\app\\.cxx\\Debug\\1s1q2b5u\\x86_64" ^
+  yolov8ncnn
